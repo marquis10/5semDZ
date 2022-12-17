df0b2122e13eaa0f943fab4ac94f8f5d8e35fe01
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-17 18:05:35 +0500

    34z_last

--- 34z/Program.cs
+++ 34z/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{    
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(99,1000);
+}        
+    
+void ReleaseArray(int[] array)
+{
+    int Count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            Count++;
+    }
+    Console.Write(Count);
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
